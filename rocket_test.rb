a5d3e2848b53bd4c4752fb61b3209a1f8792331c
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-27 11:51:27 -0400

    Test name change

--- rocket_test.rb
+++ rocket_test.rb
@@ -3,4 +3,15 @@ require_relative "rocket"
 
 class RocketTest < Minitest::Test
   # Write your tests here!
+
+  def setup
+    @rocket = Rocket.new
+  end
+
+  def test_name_change
+    @rocket.name = "Bird"
+    result = @rocket.name
+    expected = "Bird"
+    assert_equal(expected, result)
+  end
 end
